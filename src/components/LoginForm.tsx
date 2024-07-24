b6189008c6eea073eba2c5fde1ea9c1621139ced
sohyeonAn <ansh4393@gmail.com>
2024-07-24 17:25:44 +0900

    refactor: LoginForm에 react-hook-form 적용

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,55 +1,52 @@
-import { ChangeEvent, useCallback, useMemo, useState } from 'react'
-
 import { FormValues } from '@models/signin'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
 export default function LoginForm() {
-  const [formValues, setFormValues] = useState<FormValues>({
-    email: '',
-    password: '',
-  })
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<FormValues>()
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
+  const onSubmit = (data: FormValues) => {
     // @TODO: 로그인 처리
-    console.log('로그인', formValues)
+    console.log('로그인', data)
   }
 
-  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setFormValues((prevFormValues) => ({
-      ...prevFormValues,
-      [e.target.name]: e.target.value,
-    }))
-  }, [])
-
-  const errors = useMemo(() => validate(formValues), [formValues])
-  const isAvailableSubmit = Object.keys(errors).length === 0
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h1>로그인</h1>
       <div>
         <label htmlFor="email">이메일</label>
         <input
           type="email"
-          name="email"
           id="email"
-          onChange={handleChange}
-          required
+          {...register('email', {
+            pattern: {
+              value:
+                /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
+              message: '이메일 형식을 확인해주세요',
+            },
+            required: true,
+          })}
         />
       </div>
       <div>
         <label htmlFor="password">비밀번호</label>
         <input
           type="password"
-          name="password"
           id="password"
-          onChange={handleChange}
-          required
+          {...register('password', {
+            minLength: {
+              value: 8,
+              message: '비밀번호를 8글자 이상 입력해주세요',
+            },
+            required: true,
+          })}
         />
       </div>
-      <button type="submit" disabled={isAvailableSubmit === false}>
+      <button type="submit" disabled={isValid === false}>
         로그인
       </button>
       <div>
@@ -59,21 +56,4 @@ export default function LoginForm() {
       </div>
     </form>
   )
-}
-
-function validate(formValues: FormValues) {
-  let errors: Partial<FormValues> = {}
-
-  const emailValidRegex =
-    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/
-
-  if (!formValues.email?.match(emailValidRegex)) {
-    errors.email = '이메일 형식을 확인해주세요'
-  }
-
-  if (formValues.password.length < 8) {
-    errors.password = '비밀번호를 8글자 이상 입력해주세요'
-  }
-
-  return errors
 }
