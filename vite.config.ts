f831549236e2baf3c1a5e07577c4ef0af312dbcf
sohyeonAn <ansh4393@gmail.com>
2024-07-24 12:01:31 +0900

    refactor: alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+    },
   },
 })
