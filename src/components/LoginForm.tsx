d5753f47203e69b821c19dcf8137aa1c15dab574
sohyeon-VIRNECT <ash_h@virnect.com>
2024-07-25 12:07:19 +0900

    refactor: LoginForm을 yup 사용하여 검증하도록 변경

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,13 +1,29 @@
 import { FormValues } from '@models/signin'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .matches(
+      /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
+      '이메일 형식을 확인해주세요',
+    )
+    .required(),
+  password: yup
+    .string()
+    .min(8, '비밀번호를 8글자 이상 입력해주세요')
+    .required(),
+})
 
 export default function LoginForm() {
   const {
     register,
     handleSubmit,
     formState: { isValid },
-  } = useForm<FormValues>()
+  } = useForm<FormValues>({ resolver: yupResolver(schema) })
 
   const onSubmit = (data: FormValues) => {
     // @TODO: 로그인 처리
@@ -19,32 +35,11 @@ export default function LoginForm() {
       <h1>로그인</h1>
       <div>
         <label htmlFor="email">이메일</label>
-        <input
-          type="email"
-          id="email"
-          {...register('email', {
-            pattern: {
-              value:
-                /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
-              message: '이메일 형식을 확인해주세요',
-            },
-            required: true,
-          })}
-        />
+        <input type="email" id="email" {...register('email')} />
       </div>
       <div>
         <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          id="password"
-          {...register('password', {
-            minLength: {
-              value: 8,
-              message: '비밀번호를 8글자 이상 입력해주세요',
-            },
-            required: true,
-          })}
-        />
+        <input type="password" id="password" {...register('password')} />
       </div>
       <button type="submit" disabled={isValid === false}>
         로그인
