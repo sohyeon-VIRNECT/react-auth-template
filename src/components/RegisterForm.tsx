f326333356f2aa28b4d0989e710eafe70efcd6f6
sohyeon-VIRNECT <ash_h@virnect.com>
2024-07-25 18:22:52 +0900

    refactor: RegisterForm을 zod 사용하여 검증하도록 변경

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,14 +1,39 @@
 import { FormValues } from '@models/register'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const schema = z
+  .object({
+    email: z
+      .string()
+      .regex(
+        /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
+        '이메일 형식을 확인해주세요',
+      ),
+    password: z
+      .string()
+      .min(8, { message: '비밀번호를 8글자 이상 입력해주세요' }),
+    passwordConfirm: z
+      .string()
+      .min(8, { message: '비밀번호를 8글자 이상 입력해주세요' }),
+    nickname: z
+      .string()
+      .min(2, { message: '닉네임을 2글자 이상 입력해주세요' }),
+  })
+  .required()
+  .refine((data) => data.password === data.passwordConfirm, {
+    path: ['passwordConfirm'],
+    message: '비밀번호가 일치하지 않습니다.',
+  })
 
 export default function RegisterForm() {
   const {
     register,
     handleSubmit,
-    getValues,
     formState: { isValid, errors },
-  } = useForm<FormValues>()
+  } = useForm<FormValues>({ resolver: zodResolver(schema), mode: 'onChange' })
 
   const onSubmit = (data: FormValues) => {
     // @TODO: 회원가입 처리
@@ -20,33 +45,12 @@ export default function RegisterForm() {
       <h1>회원가입</h1>
       <div>
         <label htmlFor="email">이메일</label>
-        <input
-          type="email"
-          id="email"
-          {...register('email', {
-            pattern: {
-              value:
-                /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/,
-              message: '이메일 형식을 확인해주세요',
-            },
-            required: true,
-          })}
-        />
+        <input type="email" id="email" {...register('email')} />
         {errors.email && <p>{errors.email.message}</p>}
       </div>
       <div>
         <label htmlFor="password">비밀번호</label>
-        <input
-          type="password"
-          id="password"
-          {...register('password', {
-            minLength: {
-              value: 8,
-              message: '비밀번호를 8글자 이상 입력해주세요',
-            },
-            required: true,
-          })}
-        />
+        <input type="password" id="password" {...register('password')} />
         {errors.password && <p>{errors.password.message}</p>}
       </div>
       <div>
@@ -54,33 +58,13 @@ export default function RegisterForm() {
         <input
           type="password"
           id="passwordConfirm"
-          {...register('passwordConfirm', {
-            minLength: {
-              value: 8,
-              message: '비밀번호를 8글자 이상 입력해주세요',
-            },
-            validate: (value) => {
-              const { password } = getValues()
-              return password === value || '비밀번호가 일치하지 않습니다'
-            },
-            required: true,
-          })}
+          {...register('passwordConfirm')}
         />
         {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
       </div>
       <div>
         <label htmlFor="nickname">닉네임</label>
-        <input
-          type="text"
-          id="nickname"
-          {...register('nickname', {
-            minLength: {
-              value: 2,
-              message: '닉네임을 2글자 이상 입력해주세요',
-            },
-            required: true,
-          })}
-        />
+        <input type="text" id="nickname" {...register('nickname')} />
         {errors.nickname && <p>{errors.nickname.message}</p>}
       </div>
       <button type="submit" disabled={isValid === false}>
